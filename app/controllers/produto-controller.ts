570a829824bc43099bd0bbd6cae22cc5419898e6
LuixCarlos00 <107255451+LuixCarlos00@users.noreply.github.com>
2023-10-09 10:19:14 -0300

    Luiz Carlos <Semana 8> Front-End

--- app/controllers/produto-controller.ts
+++ app/controllers/produto-controller.ts
@@ -1,21 +1,56 @@
+import { Categoria } from "../models/Categoria.js";
 import { Produto } from "../models/produto.js";
 import { Produtos } from "../models/produtos.js";
 
 export class ProdutoController {
+    private inputNome : HTMLInputElement;
+    private inputPreco : HTMLInputElement;
+    private inputDescricao : HTMLInputElement;
+    private inputQuantidadeEstoque : HTMLInputElement;
+    private inputCategoria : HTMLInputElement;
+
 
     private produtos: Produtos = new Produtos();
 
     constructor() {
+        this.inputNome = document.querySelector('#Nome')
+        this.inputPreco = document.querySelector('#Preco')
+        this.inputDescricao = document.querySelector('#Descricao')
+        this.inputQuantidadeEstoque = document.querySelector('#Quantidade_Estoque')
+        this.inputCategoria = document.querySelector('#Preco')
+        
     }
 
     adiciona(): void{
         // criar produto com os dados preenchidos
-        
+        const produto = this.criarProduto();
+        this.produtos.add(produto);
+        this.produtos.get;
+        console.log(this.produtos.get);
         this.limparFormulario();
     }
 
+
+    criarProduto(){
+        const nome  = this.inputNome.value;
+        const Descricao  = this.inputDescricao.value;
+        const Categoria  = this.inputCategoria.value;
+
+        const Preco = parseInt(this.inputPreco.value);
+        const Quantidade_Estoque = parseInt(this.inputQuantidadeEstoque.value);
+
+        return new Produto(nome,Preco,Descricao,Quantidade_Estoque,Categoria);
+
+    }
+
+
+
     limparFormulario() {
-        // limpar todos os campos do formulário
+       this.inputNome.value ='';
+       this.inputDescricao.value = '';
+       this.inputPreco.value ='';
+       this.inputQuantidadeEstoque.value='';
+       this.inputCategoria.value=''; 
     }
 
 }
--- app/models/produto.ts
+++ app/models/produto.ts
@@ -1,5 +1,46 @@
+import { Categoria } from "./Categoria.js";
+
+
+
 export class Produto {
 
+
+    
+    constructor (private _Nome: String ,
+                 private _Preco : number ,
+                 private _Descicao: String ,
+                 private _Quantidade_Estoque : number,
+                 private CategoriaID :Categoria ){
+
+
+
+                 }
+
+                 
+    get Nome(): String {
+        return this._Nome;
+    }
+
+
+    get Preco (): number{
+        return this._Preco;
+    }
+                
+    get Descricao(): String {
+        return this._Descicao;
+    }
+
+
+    get Quantidade_Estoque (): number {
+        return this._Quantidade_Estoque;
+    }
+                
+    get CategoriaId(): Categoria {
+        return this.CategoriaID;
+    }
+
+
+ 
 
 
 }
