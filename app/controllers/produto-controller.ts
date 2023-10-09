d85c1a73eadb690640b36a34a915b68bfeeb18a2
Marcos Antônio da Silva <marcos@uniformg.edu.br>
2023-10-09 10:31:10 -0300

    Foram implementados os métodos e os atributos no produto-controller, foram criados os atributos dentro da entidade produto e instalado as dependência do projeto atraves do npm install. Criei a entidade categoria que será usada a posteriore.

--- app/controllers/produto-controller.ts
+++ app/controllers/produto-controller.ts
@@ -3,19 +3,49 @@ import { Produtos } from "../models/produtos.js";
 
 export class ProdutoController {
 
+    private inputNome: HTMLInputElement;
+    private inputPreco: HTMLInputElement;
+    private inputDescricao: HTMLInputElement;
+    private inputQuantidadeEstoque: HTMLInputElement;
+    private inputCategoria: HTMLInputElement;
     private produtos: Produtos = new Produtos();
 
     constructor() {
+        this.inputNome = document.querySelector('#nome');
+        this.inputPreco = document.querySelector('#preco');
+        this.inputDescricao = document.querySelector('#descricao');
+        this.inputQuantidadeEstoque = document.querySelector('#quantidadeEstoque');
+        this.inputCategoria = document.querySelector('#categoria');
     }
 
     adiciona(): void{
         // criar produto com os dados preenchidos
+       const produto = this.criaProduto();
+       this.produtos.add(produto);
+       console.log(this.produtos.get());
         
         this.limparFormulario();
     }
 
+    criaProduto(): Produto {
+       const nome = this.inputNome.value;
+       const preco = parseFloat(this.inputPreco.value);
+       const descricao = this.inputDescricao.value;
+       const quantidadeEstoque = parseInt(this.inputQuantidadeEstoque.value);
+       const categoria = this.inputCategoria.value;
+       
+       return new Produto(nome, preco, descricao, quantidadeEstoque, categoria);
+    }
+
+
     limparFormulario() {
         // limpar todos os campos do formulário
+        this.inputNome.value = '';
+        this.inputPreco.value = '';
+        this.inputDescricao.value = '';
+        this.inputQuantidadeEstoque.value = '';
+        this.inputCategoria.value = '';
+        this.inputNome.focus();
     }
 
 }
