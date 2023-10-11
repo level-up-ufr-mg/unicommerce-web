2d0bc8761cef4e1283441a961d0e125df2025b73
Dayane Costa <47676657+DayaneLopes@users.noreply.github.com>
2023-10-11 19:52:19 -0300

    Semana 8

--- app/app.ts
+++ app/app.ts
@@ -7,3 +7,4 @@ form.addEventListener('submit', event => {
     event.preventDefault();
     produtoController.adiciona();
 });
+
--- app/controllers/produto-controller.ts
+++ app/controllers/produto-controller.ts
@@ -1,21 +1,62 @@
 import { Produto } from "../models/produto.js";
 import { Produtos } from "../models/produtos.js";
+import { MensagemView } from "../views/mensagem-view.js";
+import { ProdutosView } from "../views/produto-view.js";
 
 export class ProdutoController {
+
+    private inputNome : HTMLInputElement;
+    private inputPreco : HTMLInputElement;
+    private inputDescricao : HTMLInputElement;
+    private inputQuantidade : HTMLInputElement;
+    private inputCategoria : HTMLInputElement;
+    private produtoView = new ProdutosView('#produtoView');
+    private mensagemView = new MensagemView('#mensagemView');
 
     private produtos: Produtos = new Produtos();
 
     constructor() {
+        this.inputNome = document.querySelector('#nome');
+        this.inputPreco = document.querySelector('#preco');
+        this.inputDescricao = document.querySelector('#descricao');
+        this.inputQuantidade = document.querySelector('#quantidadeEstoque');
+        this.inputCategoria = document.querySelector('#categoria');
+        this.produtoView.update(this.produtos);
+
     }
 
-    adiciona(): void{
-        // criar produto com os dados preenchidos
-        
+    public adiciona(): void {
+
+        const produto = this.criaProduto();
+        this.produtos.add(produto);
         this.limparFormulario();
+        this.atualizaView();
     }
 
-    limparFormulario() {
+    private criaProduto(): Produto {
+
+        const nome: string = this.inputNome.value;
+        const preco: number = parseFloat(this.inputPreco.value);
+        const descricao: string = this.inputDescricao.value;
+        const quantidade: number = parseInt(this.inputQuantidade.value);
+        const categoria: string =  this.inputCategoria.value;
+
+        return new Produto(nome, preco, descricao, quantidade, categoria);
+
+    }
+
+    private limparFormulario(): void {
         // limpar todos os campos do formulário
+        this.inputNome.value = '';
+        this.inputPreco.value = '';
+        this.inputDescricao.value = '';
+        this.inputQuantidade.value = '';
+        this.inputCategoria.value = '';
+        this.inputNome.focus();
     }
 
+    private atualizaView(): void{
+        this.produtoView.update(this.produtos);
+        this.mensagemView.update('Produto cadastrado com sucesso!');
+    } 
 }
