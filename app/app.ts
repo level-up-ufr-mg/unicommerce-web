c62bd988a62cc1a8769e5615f28e61675cd277b3
Henrique Eustaquio Pinto <henriqueeustaquio@uniformg.edu.br>
2023-10-16 22:53:46 -0300

    Semana08 _ EntregaSemana08

--- app/app.ts
+++ app/app.ts
@@ -1,9 +1,22 @@
 import { ProdutoController } from "./controllers/produto-controller.js";
 
-const produtoController = new ProdutoController();
-const form = document.querySelector(".form");
+const controller = new ProdutoController();
+const form = document.querySelector('.form');
+form.addEventListener('submit', event => {
+    controller.adiciona();
+});
 
+
+
+//import { ProdutoController } from "./controllers/produto-controller.js";
+//import { Produto } from "./models/produto.js";
+
+//const produto = new Produto (1,"monitor",10,"Monitor 22 pol",10,"informatica");
+//console.log(produto);
+/*const produtoController = new ProdutoController();
+const form = document.querySelector(".form");
 form.addEventListener('submit', event => {
     event.preventDefault();
     produtoController.adiciona();
 });
+*/
